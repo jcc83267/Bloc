02f0d49e64c3cdeb3a9993cf68918f23a4b66c4f
James Canlas <jamescanlasjr@yahoo.com>
2021-03-25 19:46:05 -0700

    added api/user with crud method and have api/login

--- controllers/api/post-routes.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const { Post, User, Vote, Comment } = require('../../models');
-const { sequelize } = require('../../models/User');
-
-
-module.exports = router;
--- models/User.js
+++ models/User.js
@@ -38,15 +38,15 @@ User.init(
   },
   {
     hooks: {
-  async beforeCreate(newUserData) {
-    newUserData.password = await bcrypt.hash(newUserData.password, 10);
-    return newUserData;
-  },
-  // if wanted to update password use this 
-  async beforeUpdate(updatedUserData) {
-    updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
-    return updatedUserData;
-    }
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+      // if wanted to update password use this 
+      async beforeUpdate(updatedUserData) {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        return updatedUserData;
+      }
     },
     sequelize,
     timestamps: false,
