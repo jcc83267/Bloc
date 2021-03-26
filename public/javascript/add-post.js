7f285709afad4733a22bc7b7af54af8fa3faf695
AlexDeLarge <jmrendon48@gmail.com>
2021-03-26 01:00:07 -0700

    added fetch with credentials

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -5,6 +5,36 @@ async function newFormHandler(event) {
   const post_content = document.querySelector('input[name="post-content"]').value;
   console.log("button clicked.");
 
+  // var getUserRepos = function(user) {
+  //   // format the github api url
+  //   var apiUrl = "https://api.github.com/users/" + user + "/repos";
+  
+  //   // make a request to the url
+  //   fetch(apiUrl).then(function(response) {
+  //     response.json().then(function(data) {
+  //       console.log(data);
+  //     });
+  //   });
+  // };
+  
+  let apiUrl = "https://api.twitch.tv/helix/games?name=" + title;
+
+  fetch(apiUrl, {
+    method: "GET",
+    headers: {
+      "Client-ID": "w6k0p7kqfipr0j3xuj55q2z85vrs57",
+      "Authorization": "Bearer t3brkytbcv2i1175txe20x1epa3lj6"
+    }
+    })
+      .then(function(response) {
+        response.json()
+          .then(function(data) {
+            console.log(data);
+          });
+      });
+
+  
+
   // const response = await fetch(`/api/posts`, {
   //   method: "POST",
   //   body: JSON.stringify({
