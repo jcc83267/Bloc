2623f8e5b1d0ba73737c7e9ade26d9e5edbc78d5
AlexDeLarge <jmrendon48@gmail.com>
2021-03-26 21:43:10 -0700

    retreived artwork from twitch api

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -4,22 +4,10 @@ async function newFormHandler(event) {
   const title = document.querySelector('input[name="post-title"]').value;
   const post_content = document.querySelector('input[name="post-content"]').value;
   console.log("button clicked.");
-
-  // var getUserRepos = function(user) {
-  //   // format the github api url
-  //   var apiUrl = "https://api.github.com/users/" + user + "/repos";
-  
-  //   // make a request to the url
-  //   fetch(apiUrl).then(function(response) {
-  //     response.json().then(function(data) {
-  //       console.log(data);
-  //     });
-  //   });
-  // };
   
   let apiUrl = "https://api.twitch.tv/helix/games?name=" + title;
 
-  fetch(apiUrl, {
+  await fetch(apiUrl, {
     method: "GET",
     headers: {
       "Client-ID": "w6k0p7kqfipr0j3xuj55q2z85vrs57",
@@ -27,30 +15,23 @@ async function newFormHandler(event) {
     }
     })
       .then(function(response) {
-        response.json()
-          .then(function(data) {
-            console.log(data);
-          });
+          response.json()
+            .then(function(response) {
+              console.log(response);
+              if (!response.data.length) {
+                window.alert("No game found with this title. Please enter valid title.")
+              } else {
+                console.log(response);
+                console.log(response.data[0].name);
+                console.log(response.data[0].box_art_url);
+
+                let boxArtUrl = response.data[0].box_art_url;
+                let officialArtwork = boxArtUrl.split("{");
+                officialArtwork = officialArtwork[0] + "100x400.jpg"
+                console.log(officialArtwork);
+              }
+            });
       });
-
-  
-
-  // const response = await fetch(`/api/posts`, {
-  //   method: "POST",
-  //   body: JSON.stringify({
-  //     title,
-  //     post_url,
-  //   }),
-  //   headers: {
-  //     "Content-Type": "application/json",
-  //   },
-  // });
-
-  // if (response.ok) {
-  //   document.location.replace("/dashboard");
-  // } else {
-  //   alert(response.statusText);
-  // }
 }
 
 document
