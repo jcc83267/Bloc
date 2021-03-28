274253fa92ce4088d9d07a6ccfdc58ac02e30eb4
AlexDeLarge <jmrendon48@gmail.com>
2021-03-27 18:10:27 -0700

    added ability to add post from dashboard modal

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -1,10 +1,16 @@
+var savedTitle = "";
+var savedArtwork = "";
+var savedPostUrl = "";
+
 $(document).ready(function(){
   $("#search-game-btn").click(function(){
       event.preventDefault();
     
       const title = document.querySelector('input[name="post-title"]').value;
-      const post_content = document.querySelector('input[name="post-content"]').value;
+      const post_url = document.querySelector('input[name="post-url"]').value;
+      savedPostUrl = post_url;
       console.log("button clicked.");
+      console.log(savedPostUrl);
     
       let apiUrl = "https://api.twitch.tv/helix/games?name=" + title;
     
@@ -24,23 +30,51 @@ $(document).ready(function(){
               } else {
                 $("#createPostModal").modal();
                 console.log(response);
-                console.log(response.data[0].name);
-                console.log(response.data[0].box_art_url);
                 
                 let searchedTitle = $("#searched-title")
                   .html(response.data[0].name);
+                savedTitle = response.data[0].name;
 
                 let searchedArtworkUrl = response.data[0].box_art_url;
                 searchedArtworkUrl = searchedArtworkUrl.split("{");
-                searchedArtworkUrl = searchedArtworkUrl[0] + "175x300.jpg"
+                searchedArtworkUrl = searchedArtworkUrl[0] + "175x300.jpg";
+                savedArtwork = searchedArtworkUrl;
                 let searchedArtworkRender = $("#searched-artwork")
                   .attr({
                     src: searchedArtworkUrl
-                  });
+                  })
               }
             });
         });
   });
 });
+
+async function newFormHandler(event) {
+  event.preventDefault();
+
+  const title = savedTitle;
+  const post_url = savedPostUrl;
+  const post_content = document.querySelector('input[name="post-content"]').value;
+  console.log(title);
+  console.log(savedArtwork);
+  const response = await fetch(`/api/posts`, {
+    method: 'POST',
+    body: JSON.stringify({
+      title,
+      post_url
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+
+  if (response.ok) {
+    document.location.replace('/dashboard');
+  } else {
+    alert(response.statusText);
+  }
+}
+
+document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
 
 
