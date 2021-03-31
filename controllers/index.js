bd76351bf50d2622ac47eb9e57f819d2edfe5686
AlexDeLarge <jmrendon48@gmail.com>
2021-03-30 21:05:37 -0700

    rendered reviews page

--- controllers/index.js
+++ controllers/index.js
@@ -2,10 +2,12 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes.js');
 const dashboardRoutes = require('./dashboard-routes.js');
+const reviewsRoutes = require('./reviews-routes.js');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 router.use('/dashboard', dashboardRoutes);
+router.use('/reviews', reviewsRoutes);
 
 router.use((req, res) => {
   res.status(404).end();
